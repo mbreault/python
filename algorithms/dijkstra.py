00e3861e8002b0a845c8ddc6a199eb84f029d61a
mbreault <mbreault@gmail.com>
2018-02-14 09:20:12 -0500

    fixed starting point

--- algorithms/dijkstra.py
+++ algorithms/dijkstra.py
@@ -2,7 +2,9 @@
 ## Written based on pseudo code in wiki page
 ## 2/14/2018  Know it can be improved.  WIP
 
+
 import sys
+import operator
 
 def get_path(steps,start,end,path = []):
     if start == end:
@@ -14,29 +16,46 @@ def get_path(steps,start,end,path = []):
          path = get_path(steps,start,steps[end],path)
          return path
 
+## method 1 non-recursive
 def dijkstra(graph,start,end,dist,prev,unvisited):
 
+    comparisons = 0
     while unvisited:
-        vertex = unvisited.pop(0)
+        comparisons += 1
+
+        ## set starting point
+        if start in unvisited:
+            vertex = start
+            unvisited.remove(start)
+        else:
+            vertex = unvisited.pop(0)
+
         neighbors = graph[vertex]    
-        
-        for neighbor, distance in neighbors.items():
-            alt = dist[vertex] + distance
-            if alt < dist[neighbor]:
-                dist[neighbor] = alt
-                prev[neighbor] = vertex
-        
+
+        for neighbor in neighbors:
+            print(vertex,neighbor,dist,prev)
+            if neighbor in unvisited:
+                distance = neighbors[neighbor]
+                comparisons += 1
+                alt = dist[vertex] + distance
+                if alt < dist[neighbor]:
+                    dist[neighbor] = alt
+                    prev[neighbor] = vertex
+    
+    print (comparisons)
     return dist, prev   
 
-graph = {'s': {'a': 2, 'b': 1},
+graph = {
             'a': {'s': 3, 'b': 4, 'c':8},
             'b': {'s': 4, 'a': 2, 'd': 2},
             'c': {'a': 2, 'd': 7, 't': 4},
             'd': {'b': 1, 'c': 11, 't': 5},
-            't': {'c': 3, 'd': 5}}
+            's': {'a': 2, 'b': 1},
+            't': {'c': 3, 'd': 5}
+        }
 
 ## init
-start = 's'
+start = 'a'
 end = 't'
 dist = {}
 prev = {}
