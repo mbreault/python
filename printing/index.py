4877879b259675202a26e34e695c6f649429c1ad
Mike Breault <mikeb@mcrtechnologies.com>
2020-04-25 12:58:36 -0400

    print a document to Windows default printer

---
+++ printing/index.py
@@ -0,0 +1,6 @@
+## Print a document to the default Windows printer
+## from https://stackoverflow.com/questions/12723818/print-to-standard-printer-from-python
+
+import os
+
+os.startfile("TestFile.txt", "print")
