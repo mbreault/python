b7e0d8d3c32c40af33be2f53295efd52b8864f77
mbreault <mbreault@gmail.com>
2020-05-17 07:38:34 -0400

    formatting and output changes

--- homebrew-popular/index.py
+++ homebrew-popular/index.py
@@ -17,19 +17,17 @@ with open(data_file, mode='r') as infile:
 
 pp = pprint.PrettyPrinter(indent=4)
 
-URL = "https://formulae.brew.sh/api/analytics/install/30d.json"
-
+URL = "https://formulae.brew.sh/api/analytics/install-on-request/30d.json"
 r = requests.get(url = URL)
-
 data = r.json() 
+
 limit = 5  
 count_items = len(data["items"])
 count_reviewed = len(reviewed)
 
-
 top_items = [i for i in data["items"] if i['formula'] not in list(reviewed.keys())][:limit]
 for formula in top_items:
-    print(formula['formula'])
+    print(formula['formula'],formula)
 
 
 print("\n{} {} {}\n".format(limit,count_reviewed,count_items))
