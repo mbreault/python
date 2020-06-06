8ef8221d559a28ef85e72b4ae3981346625c049d
mbreault <mbreault@gmail.com>
2020-06-06 10:40:54 -0400

    async example

---
+++ async/asyncio/hello/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+## from https://docs.python.org/3/library/asyncio.html
+
+import asyncio
+
+async def main():
+    print('Hello ...')
+    await asyncio.sleep(1)
+    print('... World!')
+
+# Python 3.7+
+asyncio.run(main())
